68f196b64c4ad3b59d6073b1149d42439a072052
konstantin.korovin <cosmo.stan@yandex.com>
2021-05-21 17:39:59 +0300

    add validator locale config

--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,18 @@
 import * as yup from 'yup';
 
+yup.setLocale({
+  string: {
+    url: 'Ссылка должна быть валидным URL',
+  },
+});
+
 const schema = yup.string().url();
 
 export default (url) => {
   try {
     schema.validateSync(url);
     return '';
-  } catch {
-    return 'Ссылка должна быть валидным URL';
+  } catch (err) {
+    return err.message;
   }
 };
