487c2f661054517e0984692ccc970578fd322a73
konstantin.korovin <cosmo.stan@yandex.com>
2021-05-26 15:43:26 +0300

    refactoring

--- src/getDocumentFromUrl.js
+++ src/getDocumentFromUrl.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
-export default (url, i18n) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
+const addProxy = (url) => {
+  const proxyLink = 'https://hexlet-allorigins.herokuapp.com/';
+  const urlWithProxy = new URL('get', proxyLink);
+  urlWithProxy.searchParams.set('disableCache', 'true');
+  urlWithProxy.searchParams.set('url', url);
+  return urlWithProxy.toString();
+};
+
+export default (url, i18n) => axios.get(addProxy(url))
   .then((response) => {
     if (response.statusText === 'OK') {
       return response.data;
