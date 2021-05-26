f5f8552728c9e27e6b9ad209dca1f1042fdd7926
konstantin.korovin <cosmo.stan@yandex.com>
2021-05-26 14:24:21 +0300

    add axios library for requests

--- src/getDocumentFromUrl.js
+++ src/getDocumentFromUrl.js
@@ -1,7 +1,9 @@
-export default (url, i18n) => fetch(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${encodeURIComponent(url)}`)
+import axios from 'axios';
+
+export default (url, i18n) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
   .then((response) => {
-    if (response.ok) {
-      return response.json();
+    if (response.statusText === 'OK') {
+      return response.data;
     }
 
     throw new Error(`${i18n.t('errors.network')}`);
