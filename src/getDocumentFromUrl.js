a33ccb926cba9adb2e8007dc8ad9d0d0f15aa79e
konstantin.korovin <cosmo.stan@yandex.com>
2021-05-26 15:51:05 +0300

    revert

--- src/getDocumentFromUrl.js
+++ src/getDocumentFromUrl.js
@@ -1,14 +1,6 @@
 import axios from 'axios';
 
-const addProxy = (url) => {
-  const proxyLink = 'https://hexlet-allorigins.herokuapp.com/';
-  const urlWithProxy = new URL('get', proxyLink);
-  urlWithProxy.searchParams.set('disableCache', 'true');
-  urlWithProxy.searchParams.set('url', url);
-  return urlWithProxy.toString();
-};
-
-export default (url, i18n) => axios.get(addProxy(url))
+export default (url, i18n) => axios.get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
   .then((response) => {
     if (response.statusText === 'OK') {
       return response.data;
