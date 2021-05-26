c4666a73ea8fe1098155f4f347c1a698dc887376
konstantin.korovin <cosmo.stan@yandex.com>
2021-05-26 16:57:21 +0300

    refactoring

--- src/application.js
+++ src/application.js
@@ -1,5 +1,5 @@
 import onChange from 'on-change';
-import i18n from 'i18next';
+import i18next from 'i18next';
 import _ from 'lodash';
 import validateURL from './validator.js';
 import getDocument from './getDocumentFromUrl.js';
@@ -8,70 +8,71 @@ import { checkIsRss, addRss } from './handlers.js';
 import { parseFeeds, parsePosts } from './parsers.js';
 import watchers from './watchers.js';
 
-export default () =>
-  i18n
-    .init({
-      lng: 'ru',
-      debug: false,
-      resources,
-    })
-    .then(() => {
-      const state = {
-        form: {
-          processState: '',
-          url: '',
-          data: {
-            feeds: [],
-            posts: [],
-          },
-          error: '',
-        },
-        downloadedFeeds: [],
-      };
+export default () => {
+  const i18n = i18next.createInstance();
+  const init = i18n.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  });
 
-      const watchedState = onChange(state, watchers(state, i18n));
+  const state = {
+    form: {
+      processState: '',
+      url: '',
+      data: {
+        feeds: [],
+        posts: [],
+      },
+      error: '',
+    },
+    downloadedFeeds: [],
+  };
 
-      const updatePosts = () => {
-        watchedState.downloadedFeeds.forEach((feed) => {
-          const promises = getDocument(feed).then((doc) => {
-            const postData = parsePosts(doc);
-            const newPosts = _.differenceBy(postData, watchedState.form.data.posts.flat(), 'guid');
-            watchedState.form.data.posts.unshift(newPosts);
-            watchedState.form.processState = 'posts downloaded';
-          });
-          Promise.all([promises]).then(setTimeout(updatePosts, 5000));
-        });
-      };
+  const watchedState = onChange(state, watchers(state, i18n));
 
-      const form = document.querySelector('form');
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const url = formData.get('url');
-        const error = validateURL(url, watchedState.downloadedFeeds, i18n);
-        watchedState.form.error = error;
-
-        if (watchedState.form.error !== '') {
-          watchedState.form.processState = 'failed';
-          return;
-        }
-
-        watchedState.form.url = url;
-
-        getDocument(url, i18n)
-          .then((data) => checkIsRss(data, i18n))
-          .then((rss) => addRss(rss, watchedState, i18n))
-          .then((rss) => parseFeeds(rss, watchedState))
-          .then((rss) => {
-            watchedState.form.data.posts.unshift(parsePosts(rss));
-            watchedState.form.processState = 'posts downloaded';
-          })
-          .then(() => form.reset())
-          .then(() => updatePosts())
-          .catch((err) => {
-            watchedState.form.error = err.message;
-            watchedState.form.processState = 'failed';
-          });
+  const updatePosts = () => {
+    watchedState.downloadedFeeds.forEach((feed) => {
+      const promises = getDocument(feed).then((doc) => {
+        const postData = parsePosts(doc);
+        const newPosts = _.differenceBy(postData, watchedState.form.data.posts.flat(), 'guid');
+        watchedState.form.data.posts.unshift(newPosts);
+        watchedState.form.processState = 'posts downloaded';
       });
-    })
-    .catch(console.error);
+      Promise.all([promises]).then(setTimeout(updatePosts, 5000));
+    });
+  };
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    const error = validateURL(url, watchedState.downloadedFeeds, i18n);
+    watchedState.form.error = error;
+
+    if (watchedState.form.error !== '') {
+      watchedState.form.processState = 'failed';
+      return;
+    }
+
+    watchedState.form.url = url;
+
+    getDocument(url, i18n)
+      .then((data) => checkIsRss(data, i18n))
+      .then((rss) => addRss(rss, watchedState, i18n))
+      .then((rss) => parseFeeds(rss, watchedState))
+      .then((rss) => {
+        watchedState.form.data.posts.unshift(parsePosts(rss));
+        watchedState.form.processState = 'posts downloaded';
+      })
+      .then(() => form.reset())
+      .then(() => updatePosts())
+      .catch((err) => {
+        watchedState.form.error = err.message;
+        watchedState.form.processState = 'failed';
+      });
+  });
+
+  return init;
+};
